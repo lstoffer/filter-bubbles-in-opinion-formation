014f5d32ed42eb602769251948b567be95c99268
beatbubble <56583897+beatbubble@users.noreply.github.com>
2019-12-02 18:22:05 +0100

    Add files via upload

--- code/model_v3.py
+++ code/model_v3.py
@@ -3,21 +3,22 @@ import math as m
 import random as r
 import matplotlib.pyplot as plt
 from scipy.stats import truncnorm
+import time as t
 
 import vertex_v3 as v
 import draw_graph as dg
 import helper as h
 
-n = 100 # number of vertices (individuals)
-T = 200 # number of timesteps
-max_follow = 3 # maximum amount one can add to following in one 
+n = 50 # number of vertices (individuals)
+T = 100 # number of timesteps
+max_follow = 5 # maximum amount one can add to following in one 
 # less filter bubbles for bf -> 0 and bu -> infinity
-bu = 20
-bf = 10
+bu = 10
+bf = 5
 p_follow = lambda opinion_distance: m.exp(-bf*opinion_distance)
 p_unfollow = lambda trust: m.exp(-bu*trust)
-trust_stability = 0.99
-confidence_mean = 0.9
+trust_stability = 0.75
+confidence_mean = 0.75
 confidence_std = 0.1
 
 def init_model(n):
@@ -29,6 +30,16 @@ def init_model(n):
         vertices.append(v.Vertex(u, o, c, p_follow, p_unfollow, max_follow, trust_stability))
     return vertices
 
+def init_model_wrapper(n, confidence_mean, confidence_std, max_follow, sep, p_follow, p_unfollow, trust_stability):
+    vertices = []
+    a, b = (0.01 - confidence_mean) / confidence_std, (0.99 - confidence_mean) / confidence_std
+    for u in range(n):
+        rop = r.random()
+        o = [(1-sep)*rop, sep*(1-rop) + rop][rop >= 0.5]
+        c = truncnorm.rvs(a, b, loc = confidence_mean, scale = confidence_std)
+        vertices.append(v.Vertex(u, o, c, p_follow, p_unfollow, max_follow, trust_stability))
+    return vertices
+                       
 def simulation(vertices, T):
     # to observe opinions over time
     opinions_over_time = np.zeros((T, len(vertices)))
@@ -50,7 +61,10 @@ if __name__ == "__main__":
     vertices = init_model(n)
 
     # simulation
+    tstart = t.time()
     opinions_over_time = simulation(vertices, T)
+    tend = t.time()
+    print(r"Completed after {0:} s".format(tend-tstart))
 
     # store graph in csv-file
     #W, o = h.build_W_o(vertices)
@@ -60,9 +74,10 @@ if __name__ == "__main__":
     
     # show data over time
     plt.plot(opinions_over_time)
-    plt.title("opinion change")
-    plt.ylabel("opinion")
-    plt.xlabel("time")
+    #plt.title("opinion change")
+    plt.ylabel("Opinion")
+    plt.xlabel("Step")
+    plt.savefig(r"sample_conv.pdf")
     plt.show()
     # show confidence distribution
     c = np.zeros(n)
---
+++ code/wrapper.py
@@ -0,0 +1,72 @@
+import matplotlib.pyplot as plt
+import time as t
+from model_v3 import *
+
+
+"""
+############################################################
+Specifiy the following parameters for network initialisation.
+############################################################
+"""
+
+# number of vertices n
+# suggested n: 50 to 100. For n >> 100, runtimes are slow.
+n = 50
+
+# parameters for normal distribution of vertex confidence; mean and standard deviation
+confidence_mean = 0.75
+confidence_std = 0.1
+
+# cap on how many vertices can be followed per step (higher number -> longer runtimes typically)
+max_follow = 5
+
+# separation of opinions: creates two strands which are separated by sep
+# sep must be in [0,1]
+sep = 0.5
+
+# following and unfollowing probabilities. (higher bf, lower bu -> more filter bubbles)
+# suggested bf: 5, 10, 20. suggested bu: 5, 10, 20.
+# you can also implement different p_follow, for example a cutoff p(x) = 0 for x > x_critical
+bf = 5
+bu = 10
+p_follow = lambda opinion_distance: m.exp(-bf*opinion_distance)
+p_unfollow = lambda trust: m.exp(-bu*trust)
+
+# trust stability (lower trust stability -> more filter bubbles)
+trust_stability = 0.75
+
+"""
+############################################################
+Initialising the model with the chosen parameters.
+############################################################
+"""
+
+V = init_model_wrapper(n, confidence_mean, confidence_std, max_follow, sep, p_follow, p_unfollow, trust_stability)
+
+"""
+############################################################
+Iteration (simulation). Specifiy the number of steps.
+############################################################
+"""
+
+# number of steps
+# suggested Steps: 100 to 300.
+Steps = 100
+
+tstart = t.time()
+opinions_over_time = simulation(V, Steps)
+tend = t.time()
+
+print(r"Completed after {0:} s".format(tend-tstart))
+
+"""
+############################################################
+Plotting the opinions.
+############################################################
+"""
+
+plt.figure()
+plt.plot(opinions_over_time)
+plt.ylabel("Opinion")
+plt.xlabel("Step")
+plt.show()
